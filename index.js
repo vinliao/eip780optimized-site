c9376397d3ff9b3315cde1d39927ea417801b431
Dattabot <jonassunandar@yahoo.com>
2019-12-04 21:15:21 +0700

    add web3 estimate gas

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-const web3 = require('web3')
+let web3 = require("./config/web3Config");
+let smartContract = require('./config/eip780');
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 const inputForm = document.querySelector('.input-form')
 if (inputForm) {
-  inputForm.addEventListener('submit', evt => {
+  inputForm.addEventListener('submit', async (evt) => {
+    // alert("wtf");
     // evt.preventDefault()
     const name = document.querySelector('#name-input').value
     const age = document.querySelector('#age-input').value
     const address = document.querySelector('#address-input').value
     const phone = document.querySelector('#phone-input').value
-
     // TODO: calculate here.
+    let bytes32Name = web3.utils.fromAscii(name).padEnd(66,'0');
+    let bytes32Age = web3.utils.fromAscii(age).padEnd(66,'0');
+    let bytes32Address = address.padEnd(42,'0');
+    let bytes32Phone = web3.utils.fromAscii(phone).padEnd(66,'0');
+    let optimizedContract = new web3.eth.Contract(smartContract.optimized.ABI, smartContract.optimized.address);
+    let notOptimizedContract = new web3.eth.Contract(smartContract.notOptimized.ABI, smartContract.notOptimized.address);
+    let gasBefore = await notOptimizedContract.methods.attest(bytes32Name, bytes32Address, bytes32Age, bytes32Phone, 10).estimateGas();
+    let gasAfter = await optimizedContract.methods.attest(bytes32Name,bytes32Address , bytes32Age, bytes32Phone, 10).estimateGas();
+    gasReduced = gasBefore - gasAfter;
+    console.log("gas before ", gasBefore);
+    console.log("gas after ", gasAfter);
+    console.log("gas reduced ", gasReduced);
+    console.log("gas percentage ", gasReduced/gasBefore * 100 ," %")
     // TODO: show animation when calculating
-
+    
     // store data after calculating
-    localStorage.setItem('gasReduced', 'some value')
-    localStorage.setItem('gasBefore', 'some value')
-    localStorage.setItem('gasAfter', 'some value')
+    localStorage.setItem('gasReduced', gasReduced)
+    localStorage.setItem('gasBefore', gasBefore)
+    localStorage.setItem('gasAfter', gasAfter)
   })
 }
 
 const result = document.querySelector('.result')
 if (result) {
-  // const gasReduced = localStorage.getItem('gasReduced')
-  // const gasBefore = localStorage.getItem('gasBefore')
-  // const gasAfter = localStorage.getItem('gasAfter')
+  const gasReduced = localStorage.getItem('gasReduced')
+  const gasBefore = localStorage.getItem('gasBefore')
+  const gasAfter = localStorage.getItem('gasAfter')
 
   // Here's how you can change the value of the gas reduced title in result page
-  // document.querySelector('.result__title-number').innerHTML = gasReduced // this should be string!
+  document.querySelector('.result__title-number').innerHTML = gasReduced // this should be string!
 
   // Here's how you can change the before and after number
-  // document.querySelector('.result__optimize-number-left').innerHTML = gasBefore
-  // document.querySelector('.result__optimize-number-right').innerHTML = gasAfter
+  document.querySelector('.result__optimize-number-left').innerHTML = gasBefore
+  document.querySelector('.result__optimize-number-right').innerHTML = gasAfter
 
 }
