462b5d7d274a058d2d8a2dcbc1e50993a2f50879
Dattabot <jonassunandar@yahoo.com>
2019-12-08 22:35:10 +0700

    add gas reduced percentage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let web3 = require("./config/web3Config");
-let smartContract = require('./config/eip780');
+let smartContract = require('./config/smart-contracts');
 
 
 function sleep(ms) {
@@ -16,7 +16,7 @@ if (inputForm) {
     const age = document.querySelector('#age-input').value
     const address = document.querySelector('#address-input').value
     const phone = document.querySelector('#phone-input').value
-    // TODO: calculate here.
+    // calculating gas here.
     let bytes32Name = web3.utils.fromAscii(name).padEnd(66,'0');
     let bytes32Age = web3.utils.fromAscii(age).padEnd(66,'0');
     let bytes32Address = address.padEnd(42,'0');
@@ -29,13 +29,16 @@ if (inputForm) {
     console.log("gas before ", gasBefore);
     console.log("gas after ", gasAfter);
     console.log("gas reduced ", gasReduced);
-    console.log("gas percentage ", gasReduced/gasBefore * 100 ," %")
-    // TODO: show animation when calculating
+    let gasReducedPercentage = gasReduced/gasBefore * 100; // get percentage
+    gasReducedPercentage = Math.floor(gasReducedPercentage * 100); //get two decimals
+    gasReducedPercentage = gasReducedPercentage / 100;
+    console.log("gas percentage ", gasReducedPercentage ," %")
     
     // store data after calculating
     localStorage.setItem('gasReduced', gasReduced)
     localStorage.setItem('gasBefore', gasBefore)
     localStorage.setItem('gasAfter', gasAfter)
+    localStorage.setItem('gasReducedPercentage',gasReducedPercentage )
   })
 }
 
@@ -44,6 +47,7 @@ if (result) {
   const gasReduced = localStorage.getItem('gasReduced')
   const gasBefore = localStorage.getItem('gasBefore')
   const gasAfter = localStorage.getItem('gasAfter')
+  const gasReducedPercentage = localStorage.getItem('gasReducedPercentage')
 
   // Here's how you can change the value of the gas reduced title in result page
   document.querySelector('.result__title-number').innerHTML = gasReduced // this should be string!
@@ -51,5 +55,5 @@ if (result) {
   // Here's how you can change the before and after number
   document.querySelector('.result__optimize-number-left').innerHTML = gasBefore
   document.querySelector('.result__optimize-number-right').innerHTML = gasAfter
-
+  document.querySelector('.result__title-percentage').innerHTML = gasReducedPercentage
 }
