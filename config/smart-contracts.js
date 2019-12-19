8af755597418e52d45f70d2a809af221aab82847
Dattabot <jonassunandar@yahoo.com>
2019-12-19 10:56:12 +0700

    finalize smart contract

--- config/smart-contracts.js
+++ config/smart-contracts.js
@@ -1,27 +1,27 @@
 module.exports = {
     optimized3:{
-        address:'0xC271a4cca28750bfFFD589517862FbDBd68DE117',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address:'0xA06B803c9bf127e7ea201Cb59BdbC308820eBe8c',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
     notOptimized3:{
-        address:'0xec6E14fC1B0e91C02a5e601CA6DDB699C2BeAa3D',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address:'0xF4464d3b5A9c3d0a4a2988a7f96c2EAAEEfccDd9',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
     optimized4: {
-        address: '0x4FD14be466b447e9C57b73476E3251aa061BF7f3',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address: '0x7fdAe07AAECA713bC7CB4b4084087BFD5d257dB8',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
     notOptimized4: {
-        address: '0xF2ADc1dF6b6DBF5447bB2537Ba3FfdaFB3e79af1',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address: '0x71ACba86df1dA7eEe0c69709CbA3EFEF1a6d15c5',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
 
     optimized5: {
-        address: '0x23EB81da5512C7E6AD903B138D32C708ec51E7ba',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "phone", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address: '0xbA8e984f9B2E2AE56345777529D195417c7Ef0EF',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "phone", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
     notOptimized5: {
-        address: '0x29d469dA0B387f3D91AFbba8f6acDAf64efA821D',
-        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "topic", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "phone", "type": "bytes32" }, { "name": "value", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
+        address: '0x8c37b831B4317aEE8fF0a220346e9f537a71729A',
+        ABI: [ { "constant": false, "inputs": [ { "name": "name", "type": "bytes32" }, { "name": "email", "type": "bytes32" }, { "name": "addr", "type": "bytes32" }, { "name": "phone", "type": "bytes32" }, { "name": "title", "type": "bytes32" }, { "name": "comment", "type": "bytes32" }, { "name": "expiredTime", "type": "uint256" } ], "name": "record", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" } ]
     },
 }
