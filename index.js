d1a30c3ee4f4f23d7980f26747d46513b90148bd
Dattabot <jonassunandar@yahoo.com>
2019-12-19 10:56:54 +0700

    3 level of optimization form handling

--- index.js
+++ index.js
@@ -6,25 +6,44 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const pageForm = document.querySelector('.page-form');
 
-const inputForm = document.querySelector('.input-form')
-if (inputForm) {
-  inputForm.addEventListener('submit', async (evt) => {
-    // alert("wtf");
-    // evt.preventDefault()
-    const name = document.querySelector('#name-input').value
-    const age = document.querySelector('#age-input').value
-    const address = document.querySelector('#address-input').value
-    const phone = document.querySelector('#phone-input').value
+if (pageForm){
+  pageForm.addEventListener('click', async (evt)=>{
+    evt.preventDefault();
+    const parameters = evt.toElement.value;
+
+    window.location.replace("./form"+parameters+".html");
+  })
+}
+
+const inputForm3 = document.querySelector('.optimization-3');
+if (inputForm3) {
+  inputForm3.addEventListener('submit', async (evt) => {
+    evt.preventDefault()
+    const name = inputForm3.querySelector('#name-input').value
+    const email = inputForm3.querySelector('#email-input').value
+    const title = inputForm3.querySelector('#title-input').value
+    const comment = inputForm3.querySelector('#comment-input').value
+    console.log(name, email, title, comment);
     // calculating gas here.
     let bytes32Name = web3.utils.fromAscii(name).padEnd(66,'0');
-    let bytes32Age = web3.utils.fromAscii(age).padEnd(66,'0');
-    let bytes32Address = address.padEnd(42,'0');
-    let bytes32Phone = web3.utils.fromAscii(phone).padEnd(66,'0');
-    let optimizedContract = new web3.eth.Contract(smartContract.optimized52.ABI, smartContract.optimized52.address);
-    let notOptimizedContract = new web3.eth.Contract(smartContract.notOptimized52.ABI, smartContract.notOptimized52.address);
-    let gasBefore = await notOptimizedContract.methods.attest(bytes32Name, bytes32Address, bytes32Age, 10, bytes32Phone, 10).estimateGas();
-    let gasAfter = await optimizedContract.methods.attest(bytes32Name,bytes32Address , bytes32Age, 10, bytes32Phone, 10).estimateGas();
+    let bytes32Email = web3.utils.fromAscii(email).padEnd(66,'0');
+    let bytes32Title = web3.utils.fromAscii(title).padEnd(66,'0');
+    let bytes32Comment = web3.utils.fromAscii(comment).padEnd(66,'0');
+
+    let optimizedContract = new web3.eth.Contract(smartContract.optimized3.ABI, smartContract.optimized3.address);
+    let notOptimizedContract = new web3.eth.Contract(smartContract.notOptimized3.ABI, smartContract.notOptimized3.address);
+
+    let beforePromise = notOptimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+    let afterPromise =  optimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+
+    // TODO: put loading screen or smth
+    let [gasBefore, gasAfter] = await Promise.all([beforePromise, afterPromise]);
+    gasBefore = parseInt(gasBefore.gasUsed);
+    gasAfter = parseInt(gasAfter.gasUsed);
     gasReduced = gasBefore - gasAfter;
     console.log("gas before ", gasBefore);
     console.log("gas after ", gasAfter);
@@ -39,6 +58,102 @@ if (inputForm) {
     localStorage.setItem('gasBefore', gasBefore)
     localStorage.setItem('gasAfter', gasAfter)
     localStorage.setItem('gasReducedPercentage',gasReducedPercentage )
+    window.location.replace("./result.html");
+  })
+}
+
+// form 4
+const inputForm4 = document.querySelector('.optimization-4');
+if (inputForm4) {
+  inputForm4.addEventListener('submit', async (evt) => {
+    evt.preventDefault()
+    const name = inputForm4.querySelector('#name-input').value
+    const email = inputForm4.querySelector('#email-input').value
+    const title = inputForm4.querySelector('#title-input').value
+    const address = inputForm4.querySelector('#address-input').value
+    const comment = inputForm4.querySelector('#comment-input').value
+    // calculating gas here.
+    let bytes32Name = web3.utils.fromAscii(name).padEnd(66,'0');
+    let bytes32Email = web3.utils.fromAscii(email).padEnd(66,'0');
+    let bytes32Title = web3.utils.fromAscii(title).padEnd(66,'0');
+    let bytes32Comment = web3.utils.fromAscii(comment).padEnd(66,'0');
+    let bytes32Address = web3.utils.fromAscii(address).padEnd(66,'0');
+
+    let optimizedContract = new web3.eth.Contract(smartContract.optimized4.ABI, smartContract.optimized4.address);
+    let notOptimizedContract = new web3.eth.Contract(smartContract.notOptimized4.ABI, smartContract.notOptimized4.address);
+
+    let beforePromise = notOptimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Address, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+    let afterPromise =  optimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Address, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+
+    // TODO: put loading screen or smth
+    let [gasBefore, gasAfter] = await Promise.all([beforePromise, afterPromise]);
+    gasBefore = parseInt(gasBefore.gasUsed);
+    gasAfter = parseInt(gasAfter.gasUsed);
+    gasReduced = gasBefore - gasAfter;
+    console.log("gas before ", gasBefore);
+    console.log("gas after ", gasAfter);
+    console.log("gas reduced ", gasReduced);
+    let gasReducedPercentage = gasReduced/gasBefore * 100; // get percentage
+    gasReducedPercentage = Math.floor(gasReducedPercentage * 100); //get two decimals
+    gasReducedPercentage = gasReducedPercentage / 100;
+    console.log("gas percentage ", gasReducedPercentage ," %")
+    
+    // store data after calculating
+    localStorage.setItem('gasReduced', gasReduced)
+    localStorage.setItem('gasBefore', gasBefore)
+    localStorage.setItem('gasAfter', gasAfter)
+    localStorage.setItem('gasReducedPercentage',gasReducedPercentage )
+    window.location.replace("./result.html");
+  })
+}
+// form 5
+const inputForm5 = document.querySelector('.optimization-5');
+if (inputForm5) {
+  inputForm5.addEventListener('submit', async (evt) => {
+    evt.preventDefault()
+    const name = inputForm5.querySelector('#name-input').value
+    const email = inputForm5.querySelector('#email-input').value
+    const title = inputForm5.querySelector('#title-input').value
+    const address = inputForm5.querySelector('#address-input').value
+    const phone = inputForm5.querySelector('#phone-input').value
+    const comment = inputForm5.querySelector('#comment-input').value
+    // calculating gas here.
+    let bytes32Name = web3.utils.fromAscii(name).padEnd(66,'0');
+    let bytes32Email = web3.utils.fromAscii(email).padEnd(66,'0');
+    let bytes32Title = web3.utils.fromAscii(title).padEnd(66,'0');
+    let bytes32Comment = web3.utils.fromAscii(comment).padEnd(66,'0');
+    let bytes32Address = web3.utils.fromAscii(address).padEnd(66,'0');
+    let bytes32Phone = web3.utils.fromAscii(phone).padEnd(66,'0');
+
+    let optimizedContract = new web3.eth.Contract(smartContract.optimized5.ABI, smartContract.optimized5.address);
+    let notOptimizedContract = new web3.eth.Contract(smartContract.notOptimized5.ABI, smartContract.notOptimized5.address);
+
+    let beforePromise = notOptimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Address, bytes32Phone, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+    let afterPromise =  optimizedContract.methods.record(bytes32Name, bytes32Email, bytes32Address, bytes32Phone, bytes32Title, bytes32Comment, 10)
+    .send({from:"0xFAC4316F68389f1B1Ec40CB39113Ea8f8bfB84c9"})
+
+    // TODO: put loading screen or smth
+    let [gasBefore, gasAfter] = await Promise.all([beforePromise, afterPromise]);
+    gasBefore = parseInt(gasBefore.gasUsed);
+    gasAfter = parseInt(gasAfter.gasUsed);
+    gasReduced = gasBefore - gasAfter;
+    console.log("gas before ", gasBefore);
+    console.log("gas after ", gasAfter);
+    console.log("gas reduced ", gasReduced);
+    let gasReducedPercentage = gasReduced/gasBefore * 100; // get percentage
+    gasReducedPercentage = Math.floor(gasReducedPercentage * 100); //get two decimals
+    gasReducedPercentage = gasReducedPercentage / 100;
+    console.log("gas percentage ", gasReducedPercentage ," %")
+    
+    // store data after calculating
+    localStorage.setItem('gasReduced', gasReduced)
+    localStorage.setItem('gasBefore', gasBefore)
+    localStorage.setItem('gasAfter', gasAfter)
+    localStorage.setItem('gasReducedPercentage',gasReducedPercentage )
+    window.location.replace("./result.html");
   })
 }
 
